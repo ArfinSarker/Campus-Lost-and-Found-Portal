6371abe94d7d7126ba93106a5c8b47e5d2a7065b
Arfin Sarker <m.shamsularfinsarkernayan@gmail.com>
2026-06-07 16:41:24 +0600

    Bump version to 1.0.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.sas.lostandfound"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 7
+        versionName = "1.0.7"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         // Enable BuildConfig generation
