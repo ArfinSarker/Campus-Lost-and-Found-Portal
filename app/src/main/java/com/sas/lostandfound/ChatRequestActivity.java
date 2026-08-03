cf0ffb87b6e84a72ff15b6107e5319eee834aa97
Arfin Sarker <m.shamsularfinsarkernayan@gmail.com>
2026-08-03 14:28:57 +0600

    Enhance sent chat request night mode UI.

--- app/src/main/java/com/sas/lostandfound/ChatRequestActivity.java
+++ app/src/main/java/com/sas/lostandfound/ChatRequestActivity.java
@@ -71,7 +71,9 @@ public class ChatRequestActivity extends AppCompatActivity {
         
         com.google.android.material.appbar.AppBarLayout appBarLayout = findViewById(R.id.appBarLayout);
         if (appBarLayout != null) {
-            HeaderColorHelper.setup(this, appBarLayout, toolbar);
+            int headerColor = androidx.core.content.ContextCompat.getColor(this, R.color.chat_request_header_bg);
+            boolean isNight = (getResources().getConfiguration().uiMode & android.content.res.Configuration.UI_MODE_NIGHT_MASK) == android.content.res.Configuration.UI_MODE_NIGHT_YES;
+            HeaderColorHelper.setup(this, appBarLayout, headerColor, headerColor, !isNight);
         }
     }
 
@@ -86,11 +88,11 @@ public class ChatRequestActivity extends AppCompatActivity {
         if (itemImageUrl != null && !itemImageUrl.isEmpty()) {
             GlideApp.with(this)
                     .load(itemImageUrl)
-                    .placeholder(R.drawable.ic_package)
+                    .placeholder(R.drawable.ic_chat_request_placeholder)
                     .diskCacheStrategy(DiskCacheStrategy.ALL)
                     .into(ivItemPhoto);
         } else {
-            ivItemPhoto.setImageResource(R.drawable.ic_package);
+            ivItemPhoto.setImageResource(R.drawable.ic_chat_request_placeholder);
         }
     }
 
